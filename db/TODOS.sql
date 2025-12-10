aa9a06426efb83d54096bed8e238fbd133209720
francalendi <franca.lendi@mng.ch>
2025-12-10 09:15:24 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,3 +11,23 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+CREATE TABLE Produkt (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(15) NOT NULL,
+    Preis DECIMAL(2,2) NOT NULL
+    );
+CREATE TABLE Zutat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(15) NOT NULL,
+    Bestand BOOLEAN
+    );
+CREATE TABLE Produkt/Zutaten (
+    Produkt_id INT NOT NULL,
+    Zutaten_id INT NOT NULL,
+    FOREIGN KEY (Produkt_id) REFERENCES Produkt(id)
+    FOREIGN KEY (Zutaten_id) REFERENCES Zutaten(id)
+    );
+
+
+    
+     
